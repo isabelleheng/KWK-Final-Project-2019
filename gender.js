1404d066248462900e2b86c02777742013cc2f82
izzyheng <isabellejhheng@gmail.com>
2019-08-02 12:28:41 -0400

    KWK Final Project 2019

---
+++ gender.js
@@ -0,0 +1,36 @@
+// var cardContainer = $('.card-container');
+// var submit = $('.submit-btn');
+// var button = $('.button');
+//
+// submit.on("click", createNewStory);
+// $('.card-container').on("click", "", deleteStory);
+//
+//
+// function createNewStory() {
+//   var bar = $('.bar').val();
+//
+//   appendNewStory(bar);
+//   clearInputs();
+// }
+// function appendNewStory(bar) {
+//     cardContainer.append(`
+//     <div class="bar-card">
+//       <p>${bar}</p>
+//
+//
+//     </div>
+//   `);
+//
+//
+//   buttonFunction(event);
+// }
+// button.on("click", buttonFunction)
+// function buttonFunction(event) {
+//   console.log('buttonFunction');
+//
+// }
+//
+//
+// function clearInputs(event) {
+//   $('.bar').val("");
+// }
